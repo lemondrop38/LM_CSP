20d6c837a0707a7f3e3d33c69648f1ec78a1db85
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-02 13:30:20 -0600

    variables practice

--- notes/practices/variables_practice.py
+++ notes/practices/variables_practice.py
@@ -1,2 +1,13 @@
 # LM 6th Variables Practice
 
+name = "Lily" 
+number1 = "9"
+number2 = "56"
+breakfast = "pancakes" 
+favorite_color = "I love all the colors"
+school = "Utah County academy of Sciences"
+eye_color = "Dark brown" 
+age = "15"
+favorite_subject = "health"
+
+print(name, number1, number2, breakfast, favorite_color, school, eye_color, age, favorite_subject)
