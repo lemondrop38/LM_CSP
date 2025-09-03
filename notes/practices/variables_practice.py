f023d892060f3d72abf68e8347020a00e68bf7e8
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-03 05:15:28 +0000

    variables

--- notes/practices/variables_practice.py
+++ notes/practices/variables_practice.py
@@ -4,10 +4,13 @@ name = "Lily"
 number1 = "9"
 number2 = "56"
 breakfast = "pancakes" 
+year = "2025"
 favorite_color = "I love all the colors"
 school = "Utah County academy of Sciences"
 eye_color = "Dark brown" 
 age = "15"
 favorite_subject = "health"
 
-print(name, number1, number2, breakfast, favorite_color, school, eye_color, age, favorite_subject)
+print(name, number1, number2, breakfast, year, favorite_color, school, eye_color, age, favorite_subject)
+
+
