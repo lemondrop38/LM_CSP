a11d4407c622378e704301cc30cd66ac2c1cd784
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-03 13:22:25 -0600

    python practice

--- first_python.py
+++ first_python.py
@@ -1,4 +1,4 @@
 # LM 6th First Phython
 
-world = input ("What is your dream travel destination?\n")
-print
+world = input ("What is your dream travel destination?: ")
+print("Your dream travel destination is", world)
