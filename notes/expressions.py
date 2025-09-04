54d1bf1f85175a6ee294d057bf81290d9188650e
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-04 13:43:56 -0600

    expressions notes

---
+++ notes/expressions.py
@@ -0,0 +1,51 @@
+# LM 6th Expressions Notes
+
+# 1. What is an algorithm?
+    # An algorithm is a set of instructions to accomplish a task. An algorithm always does the same steps.
+name = input("What is your name?")
+print("Hello", name)
+
+# Algorithum for area
+length = 5
+width = 20
+area  = length * width
+print(length, "*", width, "=", area)
+
+# 2. List steps in an algorithm
+    # Step1 -Determine peices of information needed. Step2 -Process information. Step3 -Provide an output.
+
+# Algorithum for average
+    #Step1
+age_one = 14
+age_two = 14
+age_three = 14
+age_four = 15
+    #Step2
+total= age_one + age_two + age_three+ age_four
+average = total/4
+    #Step3
+print("the average age between:", age_one, "-", age_two, "-", age_three,"-", age_four, "=", average)
+    
+# 3. List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+    #
+
+# 4. List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+
+# 5. Why are expressions so important in programming?
+    #
+
+# 6. What is an expression?
+    #
+
+#Math Equations!
+num_one = 12
+num_two = 4
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multiplication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer division", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
