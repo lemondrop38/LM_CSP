bf06fa592466d23399d7caa946be0140af991e1c
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-09 17:06:16 -0600

    financial

--- notes/practices/expressions_practice.py
+++ notes/practices/expressions_practice.py
@@ -16,3 +16,64 @@ num_five = 5
 total = num_one / num_two - num_three + num_four * num_five
 print(" The answer between:", num_one, "/", num_two, "-", num_three, "+", num_four, "*", num_five, "=", total)
 
+num_one = 6
+num_two = 4
+num_three = 12
+num_four = 72
+num_five = 8
+num_six = 9
+total = num_one * num_two / num_three + num_four / num_five - num_six
+print(" The answer between:", num_one, "*", num_two, "/", num_three, "+", num_four, "/", num_five, "-", num_six, "=", total)
+
+num_one = 17
+num_two = 6
+num_three = 2
+num_four = 4
+num_five = 3
+total = (num_one - num_two / num_three) + num_four * num_five
+print(" The answer between: (", num_one, "-", num_two, "/", num_three, ")+", num_four, "*", num_five, "=", total)
+
+num_one = -2
+num_two = 1
+num_three = 4
+num_four = 2
+num_five = 2
+num_six = 6
+num_seven = 2
+num_eight = 3
+total = num_one * num_two * num_three - num_four / num_five + num_six + num_seven - num_eight
+print(" The answer between:", num_one, "*", num_two, "*", num_three, "-", num_four, "/", num_five, "+", num_six, "+", num_seven, "-", num_eight, "=", total)
+
+num_one = -1
+num_two = 3
+num_three = 4
+num_four = 7
+num_five = 5
+num_six = 2
+num_seven = 24
+num_eight = 6
+num_nine = x
+total = num_one * num_nine * num_two - num_three * num_four / num_five - num_six * num_seven / num_eight
+print(" The answer between:", num_one, "*",num_nine, "*", num_two, "-", num_three, "*", num_four, "/", num_five, "-", num_six, '*', num_seven, "/", num_eight, "=", total)
+
+num_one = 3
+num_two = 5
+num_three = 2
+num_four = 15
+num_five = 5
+num_six = 2
+num_seven = 2
+total = num_one / num_two - num_three + num_four * num_five
+print(" The answer between:", num_one, "/", num_two, "-", num_three, "+", num_four, "*", num_five, "=", total)
+
+
+
+
+
+
+#(3*5^2/15)-(5-2^2) =4
+
+#(1^4*2^2+3^3)-2^5/4 =23
+
+#(22/2-2*5)^2+(4-6/6)^2 =10
+
--- notes/practices/financial_calculator.py
+++ notes/practices/financial_calculator.py
@@ -1,3 +1,43 @@
 # LM Financial Calculator 
 
-print("(3*5**2/15)-(5-2**2)=",(3*5**2/15)-(5-2**2)) # fill in the numbers from the assingment
+income = float(input("What is your monthy income:\n"))
+morgage = float(input("What is your monthy rent/morgage:\n"))
+utilities = float(input("what is your monthly untilities:\n"))
+groceries = float(input("what is your monthly groceries:\n"))
+transportation = float(input("what is your monthly transportation:\n"))
+
+savings = income * .1
+
+total = income - (morgage + utilities + groceries + transportation + savings)
+
+print("Your rent is: $", morgage, "and that is", (morgage / income) * 100,"% of your income")
+print("Your untilities are: $", utilities, "and that is", (utilities / income) * 100, "% of your income")
+print("Your groceries are: $", groceries, "and that is", (groceries / income) * 100, "% of your income")
+print("your transportation is: $", transportation, "and that is",(transportation / income) * 100, "% of your income")
+print("Your savings: $", savings, "and that is", (income * .1), "% of your income" )
+
+print("Your spending amount is: $", total)
+      
+      
+
+#What is your monthly income: 3000
+
+#What is your monthly rent/mortgage: 1200
+
+#What is your monthly utilities: 200
+
+#What is your monthly groceries: 250
+
+#What is your monthly transportation: 500\
+
+#Your rent is $1200.00 and that is 40% of your income.
+
+#Your utilities are $200.00 and that is 7% of your income.
+
+#Your groceries are $250.00 and that is 8% of your income.
+
+#Your transportation is $500.00 and that is 17% of your income.
+
+#You should save $300.00 a month, that is 10% of your income.
+
+#You have $550.00 of spending money each month! 
