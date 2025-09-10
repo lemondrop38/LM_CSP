1c762c0e1efcec3c301d75898164df13be0fdc0e
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-10 00:56:14 +0000

    financial calculator

--- notes/practices/financial_calculator.py
+++ notes/practices/financial_calculator.py
@@ -19,25 +19,3 @@ print("Your savings: $", savings, "and that is", (income * .1), "% of your incom
 print("Your spending amount is: $", total)
       
       
-
-#What is your monthly income: 3000
-
-#What is your monthly rent/mortgage: 1200
-
-#What is your monthly utilities: 200
-
-#What is your monthly groceries: 250
-
-#What is your monthly transportation: 500\
-
-#Your rent is $1200.00 and that is 40% of your income.
-
-#Your utilities are $200.00 and that is 7% of your income.
-
-#Your groceries are $250.00 and that is 8% of your income.
-
-#Your transportation is $500.00 and that is 17% of your income.
-
-#You should save $300.00 a month, that is 10% of your income.
-
-#You have $550.00 of spending money each month! 
