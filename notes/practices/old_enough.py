c7ab0c048d590d0ea1f0717ce9cfe0bb434a6f12
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-18 13:42:04 -0600

    old enough

---
+++ notes/practices/old_enough.py
@@ -0,0 +1,14 @@
+# LM 6th Old Enough
+
+age = int(input("How old are you?"))
+
+if age >= 18 :
+    print("You are old enough to vote!")
+if age >= 16:
+    print("You are old enough to drive!")
+if age >= 15:
+    print("You are old enough for a permit!")
+if age >= 4:
+    print("You are old enough to go to school!")
+else:
+    print("You still have more to accomplish in life! You got this!")
