7db0a7ceb3379ffc6d2cb3eeb1b1bee9522a81ed
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-19 02:21:33 +0000

    updated hello

--- notes/practices/updated_hello.py
+++ notes/practices/updated_hello.py
@@ -1,26 +1,10 @@
 # LM 6th Updated Hello
 
-def welcome(info):
-    return info.strip().lower()
-name1 = input ("What is your name?\n")
-print(f"Hello {welcome(name1)}!")
+def say_hello(name):
+    print(f"Hello {name}!")
 
-name2 = input ("What is your name?\n")
-print(f"Hello {welcome(name2)}!")
-
-name3 = input ("What is your name?\n")
-print(f"Hello {welcome(name3)}!")
-
-name4 = input ("What is your name?\n")
-print(f"Hello {welcome(name4)}!")
-
-name5 = input ("What is your name?\n")
-print(f"Hello {welcome(name5)}!")
-
-welcome("Lucy")
-welcome("Mac")
-welcome("Vance")
-welcome("Mandy")
-welcome("Krew")
-
-print(f"Welcome all to my program!")
+say_hello("Austin")
+say_hello("Bonnie")
+say_hello("Whitney")
+say_hello("Benjamin")
+say_hello("Kitty")
